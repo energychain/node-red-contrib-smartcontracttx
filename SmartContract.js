7f0b0a9e31d3adb78bb933f522f8e0f0be1c755c
Thorsten Zoerner <me@thorsten-zoerner.com>
2022-01-01 13:26:32 +0100

    RC 1.0.5 - Externalized main Method into lib to allow usage of main method from within other nodes (as package)

--- SmartContract.js
+++ SmartContract.js
@@ -1,39 +1,15 @@
-const ethers = require("ethers");
-
 module.exports = function(RED) {
     function SmartContractNode(config) {
         RED.nodes.createNode(this,config);
         this.connection = RED.nodes.getNode(config.Connection);
         this.contract = RED.nodes.getNode(config.Contract);
         const node = this;
-        const storage = node.context();
 
-        node.on('input', async function(msg) {
-          let privateKey = node.connection.privateKey
-          let contractAddress = node.contract.address;
-          let abi = node.contract.ABI;
+        const Lib = require("./lib.js");
+        const lib = new Lib(node,config)
 
-          if(config.AllowInject) {
-            if((typeof msg.payload !== 'undefined') && (msg.payload !== null)) {
-              if(typeof msg.payload.privateKey !== 'undefined') privateKey = msg.payload.privateKey;
-              if(typeof msg.payload.contract !== 'undefined') contractAddress = msg.payload.contract;
-              if(typeof msg.payload.abi !== 'undefined') abi = msg.payload.abi;
-            }
-          }
-
-          const provider = new ethers.providers.JsonRpcProvider(node.connection.rpcUrl);
-          const wallet = new ethers.Wallet(privateKey,provider);
-          const instance = new ethers.Contract(contractAddress, abi, wallet);
-          if((typeof msg.payload !== 'undefined') && (typeof msg.payload.method !== 'undefined')) {
-            if(typeof msg.payload.args == 'undefined') msg.payload.args = [];
-            try {
-              let res = await instance[msg.payload.method].apply(this,msg.payload.args);
-              node.send({payload:res});
-            } catch(e) {
-              console.log(e);
-            }
-          }
-          node.status({fill:"green",shape:"dot",text:(ethers.utils.formatEther(await wallet.getBalance()) * 1).toFixed(4)+" ETH"});
+        node.on('input', function(msg) {
+            lib.input(msg);
         });
     }
     RED.nodes.registerType("SmartContract",SmartContractNode);
---
+++ lib.js
@@ -0,0 +1,37 @@
+module.exports = function(node,config) {
+  const ethers = require("ethers");
+  
+  const storage = node.context();
+
+  async function input(msg) {
+    let privateKey = node.connection.privateKey
+    let contractAddress = node.contract.address;
+    let abi = node.contract.ABI;
+
+    if(config.AllowInject) {
+      if((typeof msg.payload !== 'undefined') && (msg.payload !== null)) {
+        if(typeof msg.payload.privateKey !== 'undefined') privateKey = msg.payload.privateKey;
+        if(typeof msg.payload.contract !== 'undefined') contractAddress = msg.payload.contract;
+        if(typeof msg.payload.abi !== 'undefined') abi = msg.payload.abi;
+      }
+    }
+
+    const provider = new ethers.providers.JsonRpcProvider(node.connection.rpcUrl);
+    const wallet = new ethers.Wallet(privateKey,provider);
+    const instance = new ethers.Contract(contractAddress, abi, wallet);
+    if((typeof msg.payload !== 'undefined') && (typeof msg.payload.method !== 'undefined')) {
+      if(typeof msg.payload.args == 'undefined') msg.payload.args = [];
+      try {
+        let res = await instance[msg.payload.method].apply(this,msg.payload.args);
+        node.send({payload:res});
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    node.status({fill:"green",shape:"dot",text:(ethers.utils.formatEther(await wallet.getBalance()) * 1).toFixed(4)+" "});
+  }
+
+  return {
+    input:input
+  }
+}
